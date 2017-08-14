8d4cc6ff629b2b51e5cfc8523a176fe1e2c9dd53
Trevor Umeda <tumeda@riotgames.com>
2017-08-14 00:20:08 -0700

    having token in config

--- futaba.js
+++ futaba.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json");
 
-client.login("MzQyOTE3MzE5OTIxNjMxMjMy.DGWn6g.c-3WIigiosUno2w-RkeFkVyHw4w");
+client.login(config.token);
 
 client.on("ready", () => {
   console.log("I am ready!");
